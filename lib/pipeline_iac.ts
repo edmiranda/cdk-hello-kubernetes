c5f83c2df8cce4e90f80bf437da9a76be08e80ea
EC2 Default User <ec2-user@ip-172-31-19-187.ec2.internal>
2023-11-09 00:44:00 +0000

    adding changes

--- lib/pipeline_iac.ts
+++ lib/pipeline_iac.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class PipelineIac extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,9 +12,13 @@ export class PipelineIac extends cdk.Stack {
         const region = props?.env?.region!;
 		const codearn = ssm.StringParameter.valueFromLookup(this, 'CodeStarArn');
 		
+		const role = iam.Role.fromRoleArn(this, 'Role', `arn:aws:iam::${account}:role/cdk-iac-pipeline`, {
+		  mutable: false,
+		});
+		
         // Pipeline code goes here
-        const pipeline = new CodePipeline(this, "ApplicationPipeline", {
-			pipelineName: "ApplicationPipeline",
+        const pipeline = new CodePipeline(this, "PipelineIac", {
+			pipelineName: "PipelineIac",
 			synth: new CodeBuildStep("Synth", {
 				input: CodePipelineSource.connection(
 					"edmiranda/cdk-hello-kubernetes",
@@ -24,9 +29,10 @@ export class PipelineIac extends cdk.Stack {
                     'yarn install --frozen-lockfile',
  			        'yarn build',
 			        'npx cdk synth',
-                ]
+                ],
+                role: role
 			}),
-			//selfMutation: false
+			selfMutation: false
 		});
     }
 }
